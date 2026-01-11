2884df5f7c2af5be555bd89c74ee6312bb36240d
Asilbek Suvonov <aslwexking777@gmail.com>
2026-01-11 18:28:52 +0500

    add

---
+++ Javascript.info doc/dars4/app.js
@@ -0,0 +1,63 @@
+// =========================== Mavzu Javascriptda o'zgaruvchilar ===============================
+
+
+// let admin='John'
+// let name=admin
+// console.log(name);
+
+
+
+// 1 chi misol
+
+// function sayHi() {
+//   if (true) {
+//     var name = true;
+//   }
+//   console.log(name);
+
+// }
+
+// sayHi()
+// console.log(name);// scopedan tashqarida bo'lgani uchun ishlamadi
+
+
+// 2 chi misol
+// for (let i = 0; i < 10; i++) {
+//   var one=1
+// console.log(i); // bu yerda funksiya ishlaydi chunki forni ichida console bor [1,2,3,4,5,6,7,8,9,10]
+// }
+// console.log(i);// bu yerda ishlamaydi chunki console for ni ichida emas
+// console.log(one);
+
+// Var va Let o'zgaruvchilarning farqi
+
+// ********* 1 - farq ***********
+// let Salom='Assalomu alaykum'
+// let Salom='Hello' // bu yerda o'zgaruvchi uniikal ochilishi kerak aks holda error beryadi
+
+// var hello ='Assalomu alaykum'
+// var hello = 'Hello' // varda esa avval ochilgan o'zgaruvchilarni qayta ochsa ham error bermaydi
+
+// ********* 2 - farq ***********
+// Hoisting nima - Hoisting o'zgaruvchidan oldin o'zgaruvchini ishlatish deyiladi
+// 2 chi farq Hoistingda bo'ladi
+
+// 1. agar letda hoisting bulsa ReferenceError beradi
+// console.log(a); //referenceError qaytaradi
+// let a ='Assalomu alaykum'
+
+// 2. agar Varda hoisting qilinsa Undefined qaytaradi
+// console.log(b); //undefined qaytaradi
+
+// var b = 'Hello World'
+
+// ********* 3 - farq ***********
+// Global object bilan bog'lanish yani - Window objecti bilan
+
+// let numbers=10
+// console.log(window.numbers); //undefined qaytaradi
+
+// var num=10
+// console.log(window.num);//10 natija chiqaradi
+
+
