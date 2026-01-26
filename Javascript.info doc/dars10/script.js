261f7f4303f2548795f208711454463cdf1eda62
Asilbek Suvonov <aslwexking777@gmail.com>
2026-01-26 23:26:41 +0500

    add

---
+++ Javascript.info doc/dars10/script.js
@@ -0,0 +1,56 @@
+// let name = prompt("ismingizni kiriting");
+// let age = Number(prompt("yoshingizni kiriting"));
+
+// let result = `Qadrli ${name} siz ${2026 - age} yilda tug'ulgansiz va siz ${age * 12} oy, ${(age * 365) / 7}hafta, ${age * 365} kun,  ${age * 365 * 24} soat,  ${age * 365 * 24 * 60} daqiqa,  ${age * 365 * 24 * 60 * 60} soniya yashagansiz `;
+// console.log(result);
+
+// 2 - masala
+// const names = prompt("ismni kirit");
+// const nameArray = ["Asilbek", "Azizbek", "Bexruz"];
+
+// if (nameArray.includes(names)) {
+//   console.log(`ha, ${names} bor`);
+// } else {
+//   console.log(`Afsuski, ${names} yuq`);
+// }
+
+// const ismlar = ["Asil", "Aziz", "Bexruz"];
+
+// for (let i = 0; i < ismlar.length; i++) {
+//   let result = ismlar[1]+'bek';
+//   console.log(result);
+// }
+
+// let region = prompt("viloytni kiriying");
+
+// switch (region) {
+//   case "Fargona":
+//     console.log("Aholisi 32298323 ta ");
+//     break;
+
+//   case "Andijon":
+//     console.log("Aholisi 3224342342 ta ");
+//     break;
+
+//   case "Toshkent":
+//     console.log("Aholisi 32794723947298 ta ");
+//     break;
+
+//   case "Qashqadaryo":
+//     console.log("Aholisi 1212313131222222222 ta ");
+//     break;
+
+//   default:
+//     console.log("notugri viloyat nomi kiritildi");
+
+//     break;
+// }
+
+// let belgilar = prompt("biror narsa yozing meyor 10 ta harf");
+// let meyor = 10;
+
+// if (belgilar.length > meyor) {
+//   console.log("success");
+// } else {
+//   console.log("belgilar biroz kup");
+// }
--- Javascript.info doc/dars9/app.js
+++ Javascript.info doc/dars9/app.js
@@ -6,10 +6,35 @@
 
 // console.log(result);
 
-const mehmon = ['Asilbek', 'Azizbek','Bexruz','Dilnura','javox','bek','Zodsa','Shukur','Bekjon',]
+// const mehmon = ['Asilbek', 'Azizbek','Bexruz','Dilnura','javox','bek','Zodsa','Shukur','Bekjon',]
 
-const number= [1,2,4,5,6,7,7,7,88]
+// const number= [1,2,4,5,6,7,7,7,88]
 // let result = mehmon.concat(number) //Concat() - ikkita elementni bir biriga qushadi
 
-let result= number.push('Asilbek')
-console.log(result);
+// let result= number.push('Asilbek')
+// console.log(result);
+
+// let age =25
+
+// console.log(age == 25);
+// console.log(age == 35);
+
+// const men=['Asilbek']
+// for (let i = 0; i < 20; i++) {
+//   console.log(men);
+
+// }
+
+// let age = prompt('yoshni kirit')
+// switch (age) {
+//   case '10':
+//     console.log('siz yosh bolalar multifilimini kuring');
+//     break;
+// case '15':
+//   console.log('siz urta yoshga mansub bulgan kinolarni kuring');
+
+//   default:
+//     console.log('sizga mumkin');
+
+//     break;
+// }
