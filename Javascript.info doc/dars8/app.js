6c6d24c58849aa0c856078810c88f897fe314532
Asilbek Suvonov <aslwexking777@gmail.com>
2026-01-26 21:42:32 +0500

    zadd

---
+++ Javascript.info doc/dars8/app.js
@@ -0,0 +1,33 @@
+let email = "Asilbek@gmail.com";
+
+// indexOf() methodi - faqat birinchi kelayotgan elementni pozitsiyasini olib keladi
+// let result = email.indexOf("a");
+
+// LasIindexOf() methodi - faqat oxiririda kelayotgan elementni pozitsiyasini olib keladi
+// let result = email.lastIndexOf("a");
+
+//slice() - malum bir miqdordagi elementlarni kesib oladi
+
+// let result = email.slice(9, 10);
+
+// substring() - vazifasi slice() methodiga uxshaydi, faqat biroz teskari ishlaydi
+// let result =email.substr(0,3)
+
+// replace() - almashtirish, ya'ni uzgaruvchidagi birorta mavjud bulgan elementni boshqasiga almashtitish birinchi kelganini
+// let result =email.replace('A', 'D')
+
+// CharAt() - Slice() methodiga uxshaydi, farqi doim stringdagi birinchi harf olib beradi
+// let result = email.charAt()
+
+// toUpperCase() - barcha stringni bosh harfda yozib beradi
+
+// let result = email.toUpperCase()
+
+// trim() - bu method vazifasi bush joyni kesib olib tashlaydi
+
+// let result =email.trim()
+
+// split() - yani bu arrayda qaytarib beradi
+
+// let result = email.split('')
+// console.log(result);
