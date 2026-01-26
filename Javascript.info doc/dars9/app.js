bd6455d1608d062a166153e696de2aef1487391f
Asilbek Suvonov <aslwexking777@gmail.com>
2026-01-26 22:02:23 +0500

    add

---
+++ Javascript.info doc/dars9/app.js
@@ -0,0 +1,15 @@
+// const title = "Assalomu alaykum rastamojka qachon ochiladi";
+// const author = `Lag'mon markazi`;
+// const likes = 3400000000;
+
+// let result = `Bu post nomi ${title} ${author} tomonidan yozilgan va bu postda ${likes} bor `;
+
+// console.log(result);
+
+const mehmon = ['Asilbek', 'Azizbek','Bexruz','Dilnura','javox','bek','Zodsa','Shukur','Bekjon',]
+
+const number= [1,2,4,5,6,7,7,7,88]
+// let result = mehmon.concat(number) //Concat() - ikkita elementni bir biriga qushadi
+
+let result= number.push('Asilbek')
+console.log(result);
