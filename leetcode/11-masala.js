2354713fecaca797a4f828391626d0d2be55eed9
Asilbek <aslwexking777@gmail.com>
2026-06-21 22:11:35 +0500

    add

--- leetcode/11-masala.js
+++ leetcode/11-masala.js
@@ -1,24 +1,18 @@
-function maxIceCream(costs: number[], coins: number): number {
+function maxIceCream(costs, coins) {
     const MAX_PRICE = 100000;
- 
     const count = new Array(MAX_PRICE + 1).fill(0);
     
-
     for (const cost of costs) {
         count[cost]++;
     }
     
     let iceCreamCount = 0;
     
-    
     for (let price = 1; price <= MAX_PRICE; price++) {
         if (count[price] === 0) continue;
-        
-      
         if (coins < price) break;
         
         const countToBuy = Math.min(count[price], Math.floor(coins / price));
-        
         iceCreamCount += countToBuy;
         coins -= countToBuy * price;
     }
